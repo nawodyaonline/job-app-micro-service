0a581df79ba0050109e17200418f1e39fbd5cbff
Nawodya Jayalath <nawodyaonline@gmail.com>
2024-07-13 16:48:00 +0530

    add: Feign client configs

---
+++ job-service/src/main/java/com/incognito/job_service/job/clients/CompanyClient.java
@@ -0,0 +1,13 @@
+package com.incognito.job_service.job.clients;
+
+import com.incognito.job_service.job.external.Company;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "COMPANY-SERVICE")
+public interface CompanyClient {
+
+    @GetMapping("/companies/{id}")
+    Company getCompany(@PathVariable("id") Long id);
+}

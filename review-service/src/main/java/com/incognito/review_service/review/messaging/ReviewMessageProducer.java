36a5431a441523a5a07b92ef16ea6c0d5d27fcc3
Nawodya Jayalath <nawodyaonline@gmail.com>
2024-09-26 20:43:07 +0530

    add: Review microservice rabbitMQ configs

---
+++ review-service/src/main/java/com/incognito/review_service/review/messaging/ReviewMessageProducer.java
@@ -0,0 +1,24 @@
+package com.incognito.review_service.review.messaging;
+
+import com.incognito.review_service.review.Review;
+import com.incognito.review_service.review.dto.ReviewMessage;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+public class ReviewMessageProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+
+    public ReviewMessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Review review) {
+        ReviewMessage reviewMessage = new ReviewMessage();
+        reviewMessage.setId(review.getId());
+        reviewMessage.setTitle(review.getTitle());
+        reviewMessage.setDescription(review.getDescription());
+        reviewMessage.setRating(review.getRating());
+        reviewMessage.setCompanyId(review.getCompanyId());
+        rabbitTemplate.convertAndSend("companyRatingQueue", reviewMessage);
+    }
+}

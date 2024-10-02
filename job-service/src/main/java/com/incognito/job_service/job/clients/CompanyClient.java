93903b3462c65b1673be24da59b76322511c4869
Nawodya Jayalath <nawodyaonline@gmail.com>
2024-10-02 20:32:24 +0530

    add: Docker compose

--- job-service/src/main/java/com/incognito/job_service/job/clients/CompanyClient.java
+++ job-service/src/main/java/com/incognito/job_service/job/clients/CompanyClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "COMPANY-SERVICE",
-        url = "${company-service.url")
+        url = "${company-service.url}")
 public interface CompanyClient {
 
     @GetMapping("/companies/{id}")
